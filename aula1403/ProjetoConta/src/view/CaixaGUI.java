0ee6c2364e05c021f3e8f1e5a1088a6f047245c2
professormoraes@gmail.com <professormoraes@gmail.com>
2024-03-14 21:36:39 -0300

    Aula do dia 14 de marco

--- aula1403/ProjetoConta/src/view/CaixaGUI.java
+++ aula1403/ProjetoConta/src/view/CaixaGUI.java
@@ -5,9 +5,11 @@ import model.Caixa;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 //Nossa tela de movimento
-public class CaixaGUI extends JFrame {
+public class CaixaGUI extends JFrame implements ActionListener {
     //Atributos
     private Label lblValor;
     private Label lblSaldo;
@@ -59,26 +61,64 @@ public class CaixaGUI extends JFrame {
         cmdEntrada.setSize(dButton);
         cmdEntrada.setLocation(25,150);
         add(cmdEntrada);
+        cmdEntrada.addActionListener(this);
 
         cmdRetirada = new Button("Retirada");
         cmdRetirada.setSize(dButton);
         cmdRetirada.setLocation(180,150);
         add(cmdRetirada);
+        cmdRetirada.addActionListener(this);
 
         cmdConsulta = new Button("Consulta");
         cmdConsulta.setSize(dButton);
         cmdConsulta.setLocation(25, 185);
         add(cmdConsulta);
+        cmdConsulta.addActionListener(this);
 
         cmdSaida = new Button("Sair");
         cmdSaida.setSize(dButton);
         cmdSaida.setLocation(180,185);
         add(cmdSaida);
+        cmdSaida.addActionListener(this);
 
         txtMsg = new TextArea(null);
         txtMsg.setSize(dTextArea);
         txtMsg.setLocation(25,220);
         add(txtMsg);
 
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==cmdSaida){
+            System.exit(0);
+        }
+        if(e.getSource()==cmdConsulta){
+            //consultar o saldo
+            double saldo = caixa.getSaldo();
+            txtSaldo.setText(Double.toString(saldo));
+            txtValor.setText(null);
+            txtMsg.append("Consulta ao saldo efetuada\n");
+            return;
+        }
+        if(e.getSource()==cmdEntrada){
+            //deposito
+            double valor = Double.parseDouble(txtValor.getText());
+            caixa.depositar(valor);
+            txtMsg.append("Depósito efetuado com sucesso\n");
+            txtValor.setText(null);
+            txtSaldo.setText(null);
+            txtValor.requestFocus(); //colocar o foco no valor
+            return;
+        }
+        if(e.getSource()==cmdRetirada){
+            //retirada
+            double valor = Double.parseDouble(txtValor.getText());
+            caixa.sacar(valor);
+            txtMsg.append("Saque efetuado com sucesso\n");
+            txtValor.setText(null);
+            txtSaldo.setText(null);
+            txtValor.requestFocus(); //colocar o foco no valor
+        }
     }
 }
